c04296d03d6474a9f98771e54b568acff06c6a9a
luisrcc <luiscastillo1991@gmail.com>
2021-03-26 14:21:27 +0000

    agregando-footer-a-nueva-rama-footer

--- src/front/js/component/Footer.js
+++ src/front/js/component/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center bg-light">
